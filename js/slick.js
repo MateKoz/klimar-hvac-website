fe0d102d2de14882778a13b192f885d4dce9907a
Mateusz Kozlowsk <climatik@world.pl>
2022-04-20 15:10:07 +0200

    completed carousel

--- js/slick.js
+++ js/slick.js
@@ -14,3 +14,31 @@ $(document).ready(function () {
 
     });
 })
+
+$('.trusted__carousel').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }
+    ]
+});
