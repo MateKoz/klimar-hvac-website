06e75a7d75be7bd8739a87aa3d5e6dd82f2dfe25
Mateusz Kozlowsk <climatik@world.pl>
2022-04-28 22:18:05 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -2,16 +2,12 @@ const footerYear = document.querySelector('.footer__year');
 const hamburgerIcon = document.querySelector('.hamburger');
 const navMobile = document.querySelector('.nav-mobile');
 
-
 const menuHamburger = () => {
     hamburgerIcon.classList.toggle('is-active')
     navMobile.classList.toggle('nav-mobile--active')
 }
 
-
-
 hamburgerIcon.addEventListener('click', menuHamburger)
-
 
 // current year in footer
 currentYear = (new Date()).getFullYear();
